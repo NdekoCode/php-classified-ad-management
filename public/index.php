734ba8af3348f6031a4a54334ec20f0da2ee9796
NdekoCode <arickbulakali@gmail.com>
2023-05-28 09:06:14 +0200

    chore: add tailwindcss for style

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Autoloader;
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Libs' . DIRECTORY_SEPARATOR . 'config.php';
+require_once ROOT_PATH  . 'Autoloader.php';
+Autoloader::register();
+?>
+<h1 class="text-3xl font-bold text-gray-500">Hello
+
+</h1>
