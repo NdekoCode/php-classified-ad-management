a0ed4ed4bb36bd059de01c4f1a1f705e78e005aa
NdekoCode <arickbulakali@gmail.com>
2023-06-02 11:21:55 +0200

    refactor: remove last slash in uri

--- Core/Main.php
+++ Core/Main.php
@@ -10,5 +10,14 @@ class Main
         // Routes: http://mon-domain.local/controller/method/parameter
         // TRUE URL: http://mon-domain.local/index.php?p=controller/method/parameter
         varDumper($_GET['p'] ?? "");
+        // On recupère l'url après le nom de domaine donc le "/controller/method/parameter"
+        $url = $_SERVER['REQUEST_URI'];
+        // On verifie que l'URL n'est pas vide et qu'elle ne se termine pas par un "/"
+        if (!empty($url) && $url[-1] === '/') {
+            // On enleve le "/" à la fin
+            $url = preg_replace("/\/+$/", "", $url);
+            http_response_code(301);
+            header("Location: $url");
+        }
     }
 }
