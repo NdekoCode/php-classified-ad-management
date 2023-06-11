e09cdb9d83964494a7e637c1a14d42829668a772
NdekoCode <arickbulakali@gmail.com>
2023-06-11 17:20:50 +0200

    feat: get annonces from database and display in view

--- Libs/config.php
+++ Libs/config.php
@@ -12,8 +12,8 @@ define('DB_NAME', 'learn-php');
 define('DNS', "mysql:host=" . DB_HOST . ';dbname=' . DB_NAME . '');
 define('DB_OPTIONS', [
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ]);
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
