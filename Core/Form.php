1bfb63a78fc7dd310902c3f7683dc4f75c383d69
NdekoCode <arickbulakali@gmail.com>
2023-06-17 11:42:08 +0200

    feat: add form create and validate fields

--- Core/Form.php
+++ Core/Form.php
@@ -18,4 +18,37 @@ class Form
     {
         $this->validator = new Validator();
     }
+    /**
+     * Genere le formulaire HTML
+     *
+     * @return string
+     */
+    public function create(): string
+    {
+        return $this->formCode;
+    }
+
+    /**
+     * Valid if all fields is filled
+     *
+     * @param array $form formData Array ($_POST, $_GET)
+     * @param array $fields Required fields Array ['email','firstName','lastName',...]
+     * @return boolean
+     */
+    public function validate(array $form, array $fields): bool
+    {
+        // On parcours les champs
+        foreach ($fields as $field) {
+            // On verifie si le champs est absent ou vide dans le formulaire
+            if ($this->validator->notExist($form[$field])) {
+                // On sort en retournant false
+                return false;
+            } elseif ($field === 'email') {
+                if (!$this->validator->isValidEmail($form[$field])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
