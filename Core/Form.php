0a4e1227403c727c5cf48c9ab222b5b6729ab133
NdekoCode <arickbulakali@gmail.com>
2023-06-17 11:42:42 +0200

    refactor: add attributes generator function in formbuilder class

--- Core/Form.php
+++ Core/Form.php
@@ -51,4 +51,29 @@ class Form
         }
         return true;
     }
+    /**
+     * Add attribute to the HTML field
+     *
+     * @param array $attributes Attributes to add in assiatives array ['class'=>"text-3xl",'required'=>true]
+     * @return string The attributes
+     */
+    public function addAttributes(array $attributes): string
+    {
+        $str = "";
+        if (!empty($attributes)) {
+
+            // We list short attributes
+            $short = ['checked', 'required', 'readonly', 'multiple', 'autofocus', 'novalidate', 'formnovalidate'];
+            // We make a loop on the attributes array
+            foreach ($attributes as $attribute => $value) {
+                // if the attribute is in the shorts list array
+                if (in_array($attribute, $short) && $value === true) {
+                    $str .= " $attribute";
+                } else {
+                    $str .= " $attribute=$value";
+                }
+            }
+        }
+        return $str;
+    }
 }
